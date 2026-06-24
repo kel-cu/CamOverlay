89dfba5a5beff30cddfc322de971fa08adaa95ff
Kel <me@kelcuprum.ru>
2026-06-24 11:01:56 +0300

    26.2

--- src/main/java/ru/kelcuprum/camoverlay/mixin/CameraMixin.java
+++ src/main/java/ru/kelcuprum/camoverlay/mixin/CameraMixin.java
@@ -1,16 +1,22 @@
 package ru.kelcuprum.camoverlay.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.kelcuprum.camoverlay.CamOverlay;
 
 @Mixin(Camera.class)
 public class CameraMixin {
-    @Inject(at = @At("HEAD"), method = "getFov", cancellable = true)
-    private void getFov(CallbackInfoReturnable<Float> cir){
-        if(CamOverlay.config.getBoolean("ENABLE", false) && CamOverlay.config.getBoolean("ENABLE.SET_FOV", true)) cir.setReturnValue(CamOverlay.getFov(cir.getReturnValueF()));
+    @ModifyReturnValue(at = @At("RETURN"), method = "calculateFov")
+    private float calculateFov(float original) {
+        if(CamOverlay.config.getBoolean("ENABLE", false) && CamOverlay.config.getBoolean("ENABLE.SET_FOV", true)) return CamOverlay.getFov(original);
+        else return original;
+    }
+
+    @ModifyReturnValue(at = @At("RETURN"), method = "calculateHudFov")
+    private float calculateHudFov(float original) {
+        if(CamOverlay.config.getBoolean("ENABLE", false) && CamOverlay.config.getBoolean("ENABLE.SET_FOV", true)) return CamOverlay.getFov(original);
+        else return original;
     }
 }
